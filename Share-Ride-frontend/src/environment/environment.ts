97e3d3d55aed93bd55a0b422a22462e5b970c548
Harsh Kyada <hkyada@argusoft.com>
2023-08-01 11:31:09 +0530

    removed environment

--- Share-Ride-frontend/src/environment/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment =  {
-    production : false,
-    MapboxKey : 'pk.eyJ1IjoiaGFyc2gta3lhZGEiLCJhIjoiY2xlNDNtaG43MDh1bTNuc2ExcWhnNDh6MCJ9.kfKxswm-yRFykKWzLMgegQ',
-    URL:'http://localhost:3000/Share-Ride'
-}
